078ce6d9bf4761a8641c77f5f79eae302c49310c
ttb2k1 <ttb2k1@gmail.com>
2021-07-26 16:30:22 +0700

    Update GUI

---
+++ GUI/DanhMuc/SuatChieu_SuaSuatChieu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAn_ver5.GUI.DanhMuc
+{
+    public partial class SuatChieu_SuaSuatChieu : Form
+    {
+        public SuatChieu_SuaSuatChieu(string MaSC)
+        {
+            InitializeComponent();
+            GUI(MaSC);
+        }
+
+        public void GUI(string MaSC)
+        {
+
+        }
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
