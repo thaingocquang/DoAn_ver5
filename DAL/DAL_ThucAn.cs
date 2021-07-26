1d201d854270be42ae6978381080b4a7a1c948fd
ttb2k1 <76573933+ttb2k1@users.noreply.github.com>
2021-07-26 10:36:49 +0700

    Update DAL_ThucAn.cs

--- DAL/DAL_ThucAn.cs
+++ DAL/DAL_ThucAn.cs
@@ -30,5 +30,72 @@ namespace DoAn_ver5.DAL
             dt = DataProvider.Instance.GetRecords(query);
             return dt;
         }
+        public DataTable GetThucAnByTenThucAn(String Ten)
+        {
+            DataTable dt = new DataTable();
+            string query = "select MaThucAn, TenThucAn, LoaiThucAn  from ThucAn where  TenThucAn = N'"+ Ten +"'";
+            dt = DataProvider.Instance.GetRecords(query);
+            return dt;
+        }
+        public DataTable GetGiaTAByTenThucAn(string Ten)
+        {
+            DataTable dt = new DataTable();
+            string query = "select SP.KichCo, SP.GiaBan from Size_Price SP join ThucAn TA on SP.MaThucAn = TA.MaThucAn where TA.TenThucAn " +
+                " = N'" + Ten + "'";
+            dt = DataProvider.Instance.GetRecords(query);
+            return dt;
+        }
+        public DataTable GetThucAnByLoaiThucAn(string Loai)
+        {
+            DataTable dt = new DataTable();
+            string query = "select MaThucAn, TenThucAn, LoaiThucAn from ThucAn where LoaiThucAn = N'" + Loai +"'";
+            dt = DataProvider.Instance.GetRecords(query);
+            return dt;
+        }
+        
+        public DataTable GetThucAnByMaThucAn(string Ma)
+        {
+            DataTable dt = new DataTable();
+            string query = "select MaThucAn, TenThucAn, LoaiThucAn from ThucAn where MaThucAn =" + Ma +"";
+            dt = DataProvider.Instance.GetRecords(query);
+            return dt;
+        }
+        public DataTable GetThucAn()
+        {
+            DataTable dt = new DataTable();
+            string query = "select MaThucAn, TenThucAn, LoaiThucAn from ThucAn ";
+            dt = DataProvider.Instance.GetRecords(query);
+            return dt;
+        }
+
+        public bool InsertThucAn(string MaThucAn, string LoaiThucAn, string TenThucAn)
+        {
+            try
+            {
+                string query = "insert into ThucAn values ('" + MaThucAn + "', N'"+ LoaiThucAn +"', N'"+ TenThucAn +"')";
+                DataProvider.Instance.ExcuteDB(query);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public bool UpdateThucAn(string MaThucAn, string LoaiThucAn, string TenThucAn)
+        {
+            try
+            {
+                string query = "update ThucAn set LoaiThucAn = N'" + LoaiThucAn + "', TenThucAn = N'" + TenThucAn + "' where MaThucAn = '" + MaThucAn + "'";
+                DataProvider.Instance.ExcuteDB(query);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
     }
 }
