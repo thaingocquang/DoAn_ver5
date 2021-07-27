ed5332b8e9ec29b77ceed0bc4b783cf6bfec2139
ttb2k1 <ttb2k1@gmail.com>
2021-07-27 16:15:49 +0700

    update

--- BLL/BLL_SuatChieu.cs
+++ BLL/BLL_SuatChieu.cs
@@ -45,22 +45,9 @@ namespace DoAn_ver5.BLL
         {
             return DAL_SuatChieu.Instance.GetDoanhThuByTimePeriod(TenPhim, TuNgay, DenNgay);
         }
-        
-        //public DataTable GetSuatChieuByDate(string dt1, string dt2)
-        //{
-        //    return DAL_SuatChieu.Instance.GetSuatChieuByDate(dt1, dt2);
-        //}
-        //public DataTable GetAllSuatChieu()
-        //{
-        //    return DAL_SuatChieu.Instance.GetAllSuatChieu();
-        //}
         public DataTable GetSuatChieuByDate(string dt1, string dt2)
         {
             return DAL_SuatChieu.Instance.GetSuatChieuByDate(dt1, dt2);
-        }
-        public DataTable GetDoanhThuByTimePeriod(string TenPhim, string TuNgay, string DenNgay)
-        {
-            return DAL_SuatChieu.Instance.GetDoanhThuByTimePeriod(TenPhim, TuNgay, DenNgay);
         }
 
         public DataTable GetAllSuatChieu()
